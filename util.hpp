f7b74924b2c9aea39ece8fc363bc634e3cf1e243
chendedaima <2813936331@qq.com>
2024-09-26 01:00:20 +0800

    util

--- util.hpp
+++ util.hpp
@@ -1,13 +1,16 @@
-#include <jsoncpp/json.h>
+#pragma once
+#include <jsoncpp/json/json.h>
 #include <iostream>
 #include <sstream>
 #include <memory>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <string>
+#include <fstream>
 
 namespace ul
 {
+    // 文件操作类
     class FileUtil
     {
     public:
@@ -17,10 +20,10 @@ namespace ul
         ~FileUtil()
         {}
         
-        //检查文件是否存在
+        //判断文件是否存在
         bool Exists()
         {
-            int ret = access(_filepath, F_OK);
+            int ret = access(_filepath.c_str(), F_OK);
             if (ret != 0)
             {
                 std::cout << "file is not exists" << std::endl;
@@ -32,10 +35,11 @@ namespace ul
         //获取文件大小
         size_t Size()
         {
-            if (this->Exists == false)
+            if (this->Exists() == false)
                 return 0;
             struct stat st;
-            int ret = stat(_filepath, st);
+            //stat函数获取文件信息
+            int ret = stat(_filepath.c_str(), &st);
             if (ret != 0)
             {
                 std::cout << "get file stat failed" << std::endl;
@@ -44,7 +48,7 @@ namespace ul
             return st.st_size;
         }
 
-        //获取文件到字符串
+        //读取文件
         bool GetContent(std::string *str)
         {
             std::ifstream ifs;
@@ -54,7 +58,7 @@ namespace ul
                 std::cerr << "open file failed" << std::endl;
                 return false;
             }
-            size_t size = Size();
+            size_t size = this->Size();
             str->resize(size);
             ifs.read(&(*str)[0], size);
             if (ifs.good() == false)
@@ -67,7 +71,7 @@ namespace ul
             return true;
         }
 
-        //读取字符串到文件
+        //写入字符串到文件
         bool SetContent(std::string &str)
         {
             std::ofstream out;
@@ -88,9 +92,10 @@ namespace ul
             return true;
         }
 
+        //创建目录
         bool CreateDirectory()
         {
-            if (Exists(_filepath))
+            if (this->Exists())
                 return true;
             int ret = mkdir(_filepath.c_str(), 0777);
             if (ret != 0)
@@ -102,5 +107,40 @@ namespace ul
         }
     private:
         std::string _filepath;//文件路径
+    };
+
+    class JsonUtil
+    {
+    public:
+        // 反序列化
+        static bool Deserialize(Json::Value &root, const std::string &str)
+        {
+            Json::CharReaderBuilder crb;
+            std::unique_ptr<Json::CharReader> cr(crb.newCharReader());
+            Json::String errs;
+            bool ret = cr->parse(str.c_str(), str.c_str() + str.size(), &root, &errs);
+            if (ret == false)
+            {
+                std::cout << errs << std::endl;
+                return false;
+            }
+            return true;
+        }
+
+        // 序列化
+        static bool Serialize(Json::Value &root, std::string *str)
+        {
+            Json::StreamWriterBuilder swb;
+            std::unique_ptr<Json::StreamWriter> sw(swb.newStreamWriter());
+            std::stringstream ssr;
+            int ret = sw->write(root, &ssr);
+            if (ret != 0)
+            {
+                std::cout << "json write failed" << std::endl;
+                return false;
+            }
+            *str = ssr.str();
+            return true;
+        }
     };
 }
